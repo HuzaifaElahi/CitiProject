65525fc4d5e452f8ea19eb0b0556ea92686636f5
Ryan Guido <37124601+ryang-123@users.noreply.github.com>
2020-08-06 09:47:38 -0400

    Update app.py

--- app.py
+++ app.py
@@ -47,7 +47,7 @@ def get_users():
     return jsonify({'users': users})
 
 #PUT 
-@app.route('/user/<string:username>')
+@app.route('/user/<string:username>', methods=['PUT'])
 def update_password(username, newpassword):
     userthere = False
     for user in users:
@@ -63,7 +63,7 @@ def update_password(username, newpassword):
     
 
 #DELETE
-@app.route('/user/<string:username>')
+@app.route('/user/<string:username>', methods=['DELETE'])
 def delete_user(username):
     global users
     users = list(filter(lambda x: x['username'] != username, users))
