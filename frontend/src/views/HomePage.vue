904a6a5db1d86f2686858ac18346f888c0f7b477
IreneLXR <46698580+IreneLXR@users.noreply.github.com>
2020-08-06 18:21:27 -0400

    enable swtich pages

---
+++ frontend/src/views/HomePage.vue
@@ -0,0 +1,18 @@
+<template>
+  <div id="Home">
+  <home/>
+  </div> 
+</template>
+
+<style>
+
+</style>
+<script>
+import home from '../components/home.vue'
+export default {
+  name: 'Home',
+  components: {
+    home
+  }
+}
+</script>
---
+++ frontend/src/views/RegPage.vue
@@ -0,0 +1,15 @@
+<template>
+  <div id="reg">
+  <register/>
+  </div> 
+</template>
+
+<script>
+import register from '../components/register.vue'
+export default {
+  name: 'reg',
+  components: {
+    register
+  }
+}
+</script>
